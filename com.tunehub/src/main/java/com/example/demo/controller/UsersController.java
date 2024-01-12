8c2207b6e4765112ed4f9a0b9dbf2e4fb59db6be
HP <HP@DESKTOP-7BE4S60>
2024-01-12 16:09:31 +0530

    Tune hub

---
+++ com.tunehub/src/main/java/com/example/demo/controller/UsersController.java
@@ -0,0 +1,78 @@
+package com.example.demo.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.entities.Users;
+import com.example.demo.services.UsersService;
+
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class UsersController {
+	   @Autowired
+		UsersService service;
+		
+		@PostMapping("/registration")
+		public String addUsers(@ModelAttribute Users user) {
+			boolean userStatus = service.emailExists(user.getEmail());
+			if(userStatus == false) {
+				service.addUser(user);
+				System.out.println("user added");
+			}else {
+				System.out.println("user already exists");
+			}
+			return "home";
+		}
+		//public String addUsers(@RequestParam("username") String username,
+				//@RequestParam("email") String email,
+				//@RequestParam("password") String password,
+			   //@RequestParam("gender") String gender,
+				//@RequestParam("role") String role,
+				//@RequestParam("address") String address
+		//) {
+			//System.out.println(username+" "+email+" "+password+" "+gender+" "+role+" "+address+" ");
+			//System.out.println(user.getUsername()+" "+user.getEmail()+" "+user.getPassword()+" "+user.getGender()+" "+user.getRole()+" "+user.getAddress());
+			//service.addUsers(user);
+		  
+		@PostMapping("/validate")
+		public String validate(@RequestParam("email")String email,
+				@RequestParam("password") String password,HttpSession session) {
+			if(service.validateUser(email,password)==true) {
+				String role=service.getRole(email);
+				if(role.equals("admin")) {
+					return "adminHome";
+				}else {
+					return "customer";
+				} 
+		       }
+		      else {
+			return "login";
+		}
+		}
+          /* @GetMapping("/pay")
+           public String pay(@RequestParam String email) {
+        	   boolean paymentStatus = false; //
+        	   if(paymentStatus == true) {
+        		   Users user = service.getUser(email);
+        		   user.setPremium(true);
+        		   service.updateUser(user);
+        		   
+        	   }
+        	   return "login";
+           }*/
+           @GetMapping("/logout")
+           public String logout(HttpSession session) {
+        	   session.invalidate();
+        	   return "login";
+}	
+}
+
+	
+
